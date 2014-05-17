ad6e48d387267d0feff344fb61b77b9a3655323c
Ђорђе Живановић <djordjezivanovicva@gmail.com>
2014-05-18 00:28:07 +0200

    Друго слање.

--- AES/AES/AES/queue.c
+++ AES/AES/AES/queue.c
@@ -2,15 +2,21 @@
 #include <string.h>
 #include <stdio.h>
 
-//proverava da li je lista prazna
-int EmptyQFile(QueueFilePt ptQ) {
+// postavlja pokazivace na prvi i poslednji element liste na NULL
+void InitalizeFileQ(QueueFilePt ptQ) {
+	ptQ->begin = NULL;
+	ptQ->end = NULL;
+}
+
+//proverava da li je lista prazna, vraca 1 ako jeste, 0 inace
+int EmptyFileQ(QueueFilePt ptQ) {
 	if (ptQ->begin == NULL)
 		return 1;
 	return 0;
 }
 
-// dodaje na kraj liste
-int PushBackFile(QueueFilePt ptQ, char *nameFile) {
+// dodaje ime fajla na kraj liste, 0 vraca ako nije uspelo, 1 inace
+int PushBackFileQ(QueueFilePt ptQ, char *nameFile) {
 	NodeFilePt newNode; //novi cvor
 
 	newNode = (NodeFilePt)malloc(sizeof(NodeFile));
@@ -35,8 +41,8 @@ int PushBackFile(QueueFilePt ptQ, char *nameFile) {
 	return 1; // sve dobro proslo
 }
 
-//dodaje na pocetak liste
-int PushFrontFile(QueueFilePt ptQ, char *nameFile) {
+//dodaje na ime fajla na pocetak liste, 0 ako nije uspelo, 1 inace
+int PushFrontFileQ(QueueFilePt ptQ, char *nameFile) {
 	NodeFilePt newNode; //novi cvor
 
 	newNode = (NodeFilePt)malloc(sizeof(NodeFile));
@@ -61,14 +67,14 @@ int PushFrontFile(QueueFilePt ptQ, char *nameFile) {
 }
 
 //vraca ime prvog fajla u listi
-char *First(QueueFilePt ptQ) {
+char *FirstFileQ(QueueFilePt ptQ) {
 	if (ptQ->begin == NULL)
 		return NULL;
 	return ptQ->begin->fileName;
 }
 
 //vraca ime prvog fajla i skida ga iz liste, memorija koju koristi ovaj string nije dealocirana
-char *PopFrontFile(QueueFilePt ptQ) {
+char *PopFrontFileQ(QueueFilePt ptQ) {
 	NodeFilePt ptNode;
 	char *s;
 
@@ -85,6 +91,89 @@ char *PopFrontFile(QueueFilePt ptQ) {
 	free(ptNode);
 	return  s;
 }
+
+// vraca 1 ako je lista prazna, 0 inace
+int EmptyKeyQ(QueueKeyPt ptQ) {
+	if (ptQ->begin == NULL)
+		return 1;
+	return 0;
+}
+
+// postavlja velicinu kljuca
+void SetKeyQ(QueueKeyPt ptQ, int keySize) {
+	ptQ->keySize = keySize;
+}
+
+// stavlja kljuc na kraj liste, vraca 1 ako je uspesno, 0 ako je neuspesno
+int PushBackKeyQ(QueueKeyPt ptQ, UInt32 *key) {
+	NodeKeyPt newNode;
+
+	newNode = (NodeKeyPt)malloc(sizeof(NodeKey));
+	if (newNode == NULL) // nema memorije na heap-u
+		return 0;
+
+	memcpy(newNode->key, key, ptQ->keySize / 8);
+	newNode->next = NULL;
+
+	if (ptQ->end == NULL) {
+		ptQ->end = newNode;
+		ptQ->begin = newNode;
+	}
+	else {
+		ptQ->end->next = newNode;
+		ptQ->end = ptQ->end->next;
+	}
+	return 1; // sve dobro proslo
+}
+	
+int PushFrontKeyQ(QueueKeyPt ptQ, UInt32 *key) {
+	NodeKeyPt newNode; //novi cvor
+
+	newNode = (NodeKeyPt)malloc(sizeof(NodeKey));
+	if (newNode == NULL) // nema memorije na heap-u
+		return 0;
+
+	newNode->key = (UInt32*)malloc(ptQ->keySize);
+	if (newNode->key == NULL) // nema memorije na hipu
+		return 0;
+
+	memcpy(newNode->key, key, ptQ->keySize / 8);
+	newNode->next = NULL;
+
+	if (ptQ->end == NULL) {
+		ptQ->end = newNode;
+		ptQ->begin = newNode;
+	}
+	else {
+		newNode->next = ptQ->begin;
+		ptQ->begin = newNode;
+	}
+}
+
+UInt32 *FirstKeyQ(QueueKeyPt ptQ) {
+	if (ptQ->begin == NULL)
+		return NULL;
+	return ptQ->begin->key;
+}
+
+UInt32 *PopFrontKeyQ(QueueKeyPt ptQ) {
+	NodeKeyPt ptNode;
+	UInt32 *s;
+
+	if (ptQ->begin == NULL) {
+		printf("ERROR, LISTA JE PRAZNA");
+		return NULL;
+	}
+
+	ptNode = ptQ->begin; // cvor za brisanje
+	s = ptNode->key;
+
+	ptQ->begin = ptQ->begin->next;
+
+	free(ptNode);
+	return  s;
+}
+
 
 int main() {
 	NodeFile node;
--- AES/AES/AES/queue.h
+++ AES/AES/AES/queue.h
@@ -4,6 +4,7 @@
 * @brief Садржи дефиниције листи кључева и листи имена фајлова. 
 * Садржи функције за рад са листама @see
 */
+
 #ifndef _queue_h_
 #define _queue_h_
 #define _CRT_SECURE_NO_DEPRECATE
@@ -11,6 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef unsigned __int32 UInt32;
 
 typedef struct NodeFile NodeFile;
 
@@ -27,16 +29,48 @@ typedef struct QueueFile {
 
 typedef QueueFile *QueueFilePt;
 
-int EmptyQFile(QueueFilePt ptQ);
+typedef struct NodeKey NodeKey;
 
-int PushBackFile(QueueFilePt ptQ, char *nameFile);
+typedef NodeKey *NodeKeyPt;
 
-int PushFrontFile(QueueFilePt ptQ, char *nameFile);
+struct NodeKey {
+	UInt32 *key;
+	NodeKeyPt next;
+};
 
-char *PopBackFile(QueueFilePt ptQ);
+typedef struct QueueKey {
+	NodeKeyPt begin, end;
+	int keySize;
+} QueueKey;
 
-char *PopFrontFile(QueueFilePt ptQ);
+typedef QueueKey *QueueKeyPt;
 
+/*Lista imena fajlova*/
 
+void InitializeFileQ(QueueFilePt ptQ);
+
+int EmptyFileQ(QueueFilePt ptQ);
+
+int PushBackFileQ(QueueFilePt ptQ, char *nameFile);
+
+int PushFrontFileQ(QueueFilePt ptQ, char *nameFile);
+
+char *FirstFileQ(QueueFilePt ptQ);
+
+char *PopFrontFileQ(QueueFilePt ptQ);
+
+/* Lista kljuceva */
+
+int EmptyKeyQ(QueueKeyPt ptQ);
+
+void SetKeyQ(QueueKeyPt ptQ, int keySize);
+
+int PushBackKeyQ(QueueKeyPt ptQ, UInt32 *key);
+
+int PushFrontKeyQ(QueueKeyPt ptQ, UInt32 *key);
+
+UInt32 *FirstKeyQ(QueueKeyPt ptQ);
+
+UInt32 *PopFrontKeyQ(QueueKeyPt ptQ);
 
 #endif
